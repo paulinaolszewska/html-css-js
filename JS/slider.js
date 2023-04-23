fdea23a6721aacb63aa44137fb1d7cec5b28a766
paula0403 <paulinalipinskagdy@gmail.com>
2023-04-23 23:22:32 +0200

    comments added

--- JS/slider.js
+++ JS/slider.js
@@ -1,14 +1,18 @@
+// pobieramy wszystkie potrzebne elementy DOM za pomocą querySelector lub querySelectorAll
 const img=document.querySelector("img.slider");
 const txt=document.querySelector("h1.slider");
+// NodeList warto zamienić na tablicę, aby mieć więcej dostępnych metod
 const dots=[...document.querySelectorAll(".dots span")];
+// tworzymy tablicę obiektów do wyświetlenia kolejnych zdjęć i tekstów
 const images=[{img:"images/img1.jpg", text:"Zakhyntos"},{img:"images/img2.jpg", text:"Pier"},{img:"images/img3.jpg",text:"Maledives"}];
 let index=0;
 
 const changeDot=()=>{
+  // poszukujemy index elementu, który aktualnie posiada klasę active, funckja zwróci nam indeks
  const active=dots.findIndex(dot=>dot.classList.contains('active'));
+//  usuwamy klasę ze znalezionego elementu i dodemy tę klasę do elementu o indeksie takim samym jak aktualnie wyświetlane zdjęcie oraz tekst
  dots[active].classList.remove('active');
  dots[index].classList.add('active');
- link=document.querySelector('a');
 }
 const changeSlide=()=>{
   index++;  
@@ -21,6 +25,7 @@ const changeSlide=()=>{
 }
 const btnChange=(e)=>{
     clearInterval(idInterval);
+    // operator trójargumengowy, skrócenie if else
     e.target.className==="next"?index++:index--;
     if(index===images.length){
         index=0;
@@ -34,4 +39,5 @@ const btnChange=(e)=>{
 
 }
 let idInterval=setInterval(changeSlide,2000);
+// ustawienie nasłuchiwania na zdarzenie w pętli, na każdym elemencie
 document.querySelectorAll("button").forEach(btn=>btn.addEventListener("click",btnChange))
--- JS/timer.js
+++ JS/timer.js
@@ -1,14 +1,19 @@
+// na starcie aplikacji pobieramy wszystkie potrzebne elementy DOM do zmiennych poprzez querySelector (HTMLelement) lub querySelectorAll (NodeList)
 const start=document.querySelector("#start");
 const reset=document.querySelector("#reset");
 const timerTxt=document.querySelector("#timerTxt");
 
 let time=0;
+// inicjalizujemy flagę, która jest niezbędna do zmiany tekstu na przycisku, zadziała jak przełącznik
 let active=true;
 const stoper=()=>{
+    // console.log (zaloguj w konsoli) do debbugowania
     console.log("click");
     if(active){
      active=false;   
+    //  właściwość text content służy do zmiany wyswietlanego tekstu wewnątrz elementu
     start.textContent="Pause";
+    // set interval to funkcja czasu, która jako pierwszy parametr przyjmuję funkcję-callback, która ma być wywoływana co pewien interwał czasu, a drugim parametrem jest właśnie interwał czasu. Przypisanie setInterval do zmiennej jest konieczne, jeśli będziemy chcieli tę funkcję wyłączyć 
     id=setInterval(timer,10);
     }else{
         active=true;
@@ -18,14 +23,18 @@ const stoper=()=>{
 };
 const timer=()=>{
     time++;
+    // metoda toFixed zaokrągla wynik do 2 miejsc po przecinku
     timerTxt.textContent=(time/100).toFixed(2);
 };
 const stop = () => {
+    // clear interval jako parametr przyjmuje funkcję setInterval, która została zaimplementowana wyżej
     clearInterval(id);
     timerTxt.textContent = "---";
     start.textContent = "Start";
     active = true;
     time = 0;
   };
+
+//   addEventListener to ustawienie nasłuchiwania na zdarzenie, w tym wypadku przyciski nasłuchują na zdarzenie kliknięcia
 start.addEventListener("click", stoper);
 reset.addEventListener("click",stop);
